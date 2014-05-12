21a75416b05905af7a6b65096aa08ce8000efcca
Mahopa <mahopa@gmail.com>
2014-05-12 15:17:44 -0500

    Initial

---
+++ MVCIntro2/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using MVCIntro2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCIntro2.Controllers
+{
+    public class HomeController : Controller
+    {
+        Dog myDog = new Dog()
+        {
+            Name = "Rex",
+            Owner = "Me"
+        };
+        public ActionResult Index()
+        {
+            
+            Dog myDog = new Dog();
+            myDog.Owner = "Me";
+            List<int> myList = new List<int>() {
+            1,
+            58,
+            372
+            };
+            myList.Add(27);
+            string noise = myDog.Speak();
+            //return myList;
+             return View(myList);
+        }
+
+        public ActionResult About()
+        {
+            
+            List<Dog> myDogs = new List<Dog>();
+            myDogs.Add(myDog);
+            myDogs.Add(new Dog() { Name = "Spot", Owner = "Ed" });
+            myDogs.Add(new Dog() { Legs = 10  });
+            return View(myDogs);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Dogs()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCIntro2/Models/dog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCIntro2.Models
+{
+    /// <summary>
+    /// This is the dog class
+    /// </summary>
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Owner { get; set; }
+        public string Sound { get; set; }
+       // public int Legs { get; set; }
+        private int legs;
+
+        public int Legs
+        {
+            get { return legs; }
+            set
+            {
+                if (value <= 4)
+                { legs = value; }
+                else
+                {
+                    legs = 4;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This is the default Constructor
+        /// </summary>
+        public Dog() { }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="owner"></param>
+        public Dog(string name, string owner)
+        {
+            this.Name = name;
+            this.Owner = owner;
+            this.legs = 15;
+            this.Sound = "Woof";
+        }
+        public Dog(string sound)
+        {
+
+            this.Sound = sound;
+        }
+        public string Speak()
+        {
+            return this.Sound;
+        }
+    }
+}
